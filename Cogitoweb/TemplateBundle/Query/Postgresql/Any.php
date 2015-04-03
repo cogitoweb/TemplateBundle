0397e09df68ad7e8a0cb3c559212ca5305ab471f
2zx <paolo.cazzitti@cogitoweb.it>
2015-04-03 16:22:40 +0200

    postgresql ANY

---
+++ Cogitoweb/TemplateBundle/Query/Postgresql/Any.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Cogitoweb\TemplateBundle\Query\Postgresql;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+class ANY extends FunctionNode {
+
+    public $field;
+
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker) {
+        return 'ANY(' .
+            $this->field->dispatch($sqlWalker) . ')';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser) {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->field = $parser->StringPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
