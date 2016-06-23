aa773b519578c38a266e33f8f9a4c8c790dbe686
Daniele Artico <daniele.artico@cogitoweb.it>
2016-06-23 16:51:32 +0200

    Fixed query ordering problem with ProxyQuery

--- Cogitoweb/TemplateBundle/Datagrid/CogitowebProxyQuery.php
+++ Cogitoweb/TemplateBundle/Datagrid/CogitowebProxyQuery.php
@@ -42,70 +42,69 @@ class CogitowebProxyQuery extends ProxyQuery
 	private function getFixedQueryBuilder(QueryBuilder $queryBuilder)
 	{
 		$queryBuilderId = clone $queryBuilder;
+        // step 1 : retrieve the targeted class
+        $from = $queryBuilderId->getDQLPart('from');
+        $class = $from[0]->getFrom();
+        // step 2 : retrieve the column id
+        $idName = current($queryBuilderId->getEntityManager()->getMetadataFactory()->getMetadataFor($class)->getIdentifierFieldNames());
+        // step 3 : retrieve the different subjects id
+        $rootAliases = $queryBuilderId->getRootAliases();
+        $rootAlias = $rootAliases[0];
+        $select = sprintf('%s.%s', $rootAlias, $idName);
+        $queryBuilderId->resetDQLPart('select');
+        $queryBuilderId->add('select', 'DISTINCT '.$select);
+        $queryBuilderId = $this->preserveSqlOrdering($queryBuilderId);
+        $results = $queryBuilderId->getQuery()->execute(array(), Query::HYDRATE_ARRAY);
+        $idx = array();
+        $connection = $queryBuilder->getEntityManager()->getConnection();
+        foreach ($results as $id) {
+            $idx[] = $connection->quote($id[$idName]);
+        }
+        // step 4 : alter the query to match the targeted ids
+        if (count($idx) > 0) {
+            $queryBuilder->andWhere(sprintf('%s IN (%s)', $select, implode(',', $idx)));
+            $queryBuilder->setMaxResults(null);
+            $queryBuilder->setFirstResult(null);
+        }
+        return $queryBuilder;
+	}
 
-		// step 1 : retrieve the targeted class
-		$from  = $queryBuilderId->getDQLPart('from');
-		$class = $from[0]->getFrom();
-		$metadata = $queryBuilderId->getEntityManager()->getMetadataFactory()->getMetadataFor($class);
-
-		// step 2 : retrieve identifier columns
-		$idNames = $metadata->getIdentifierFieldNames();
-
-		// step 3 : retrieve the different subjects ids
-		$selects = array();
-		$idxSelect = '';
-		foreach ($idNames as $idName) {
-			$select = sprintf('%s.%s', $queryBuilderId->getRootAlias(), $idName);
-			// Put the ID select on this array to use it on results QB
-			$selects[$idName] = $select;
-			// Use IDENTITY if id is a relation too. See: http://doctrine-orm.readthedocs.org/en/latest/reference/dql-doctrine-query-language.html
-			// Should work only with doctrine/orm: ~2.2
-			$idSelect = $select;
-			if ($metadata->hasAssociation($idName)) {
-				$idSelect = sprintf('IDENTITY(%s) as %s', $idSelect, $idName);
-			}
-			$idxSelect .= ($idxSelect !== '' ? ', ' : '') . $idSelect;
-		}
-		// Cogitoweb: also reset 'orderBy' part to avoid DBMS errors
-	//        $queryBuilderId->resetDQLPart('select');
-		$queryBuilderId->resetDQLParts(['select', 'orderBy']);
-		$queryBuilderId->add('select', 'DISTINCT '.$idxSelect);
-
-		// for SELECT DISTINCT, ORDER BY expressions must appear in idxSelect list
-		/* Consider
-			SELECT DISTINCT x FROM tab ORDER BY y;
-		For any particular x-value in the table there might be many different y
-		values.  Which one will you use to sort that x-value in the output?
-		*/
+	/**
+	 * Generates new QueryBuilder for Postgresql or Oracle if necessary.
+	 *
+	 * @param $queryBuilder QueryBuilder
+	 *
+	 * @return QueryBuilder
+	 */
+	public function preserveSqlOrdering(QueryBuilder $queryBuilder)
+	{
+		$rootAliases = $queryBuilder->getRootAliases();
+		$rootAlias = $rootAliases[0];
+		// for SELECT DISTINCT, ORDER BY expressions must appear in select list
+		// Consider SELECT DISTINCT x FROM tab ORDER BY y;
+		// For any particular x-value in the table there might be many different y
+		// values. Which one will you use to sort that x-value in the output?
 		// todo : check how doctrine behave, potential SQL injection here ...
 		if ($this->getSortBy()) {
 			$sortBy = $this->getSortBy();
-			if (strpos($sortBy, '.') === false) { // add the current alias
-				$sortBy = $queryBuilderId->getRootAlias() . '.' . $sortBy;
+			if (strpos($sortBy, '.') === false) {
+				// add the current alias
+				$sortBy = $rootAlias.'.'.$sortBy;
 			}
 			$sortBy .= ' AS __order_by';
-			$queryBuilderId->addSelect($sortBy);
+			$queryBuilder->addSelect($sortBy);
 		}
-
-		$results    = $queryBuilderId->getQuery()->execute(array(), Query::HYDRATE_ARRAY);
-		$idxMatrix  = array();
-		foreach ($results as $id) {
-			foreach ($idNames as $idName) {
-				$idxMatrix[$idName][] = $id[$idName];
+		// For any orderBy clause defined directly in the dqlParts
+		$dqlParts = $queryBuilder->getDqlParts();
+		if ($dqlParts['orderBy'] && count($dqlParts['orderBy'])) {
+			$sqlOrderColumns = array();
+			foreach ($dqlParts['orderBy'] as $part) {
+				foreach ($part->getParts() as $orderBy) {
+					$sqlOrderColumns[] = preg_replace("/\s+(ASC|DESC)$/i", '', $orderBy);
+				}
 			}
+			$queryBuilder->addSelect(implode(', ', $sqlOrderColumns));
 		}
-
-		// step 4 : alter the query to match the targeted ids
-		foreach ($idxMatrix as $idName => $idx) {
-			if (count($idx) > 0) {
-				$idxParamName = sprintf('%s_idx', $idName);
-				$queryBuilder->andWhere(sprintf('%s IN (:%s)', $selects[$idName], $idxParamName));
-				$queryBuilder->setParameter($idxParamName, $idx);
-				$queryBuilder->setMaxResults(null);
-				$queryBuilder->setFirstResult(null);
-			}
-		}
-
 		return $queryBuilder;
 	}
 }
