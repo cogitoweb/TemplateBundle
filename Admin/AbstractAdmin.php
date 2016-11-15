0c30cb68f2ec23a18734db08e1f29f35ca89adb4
Daniele Artico <daniele.artico@cogitoweb.it>
2016-11-15 14:36:21 +0100

    Addedd object informations method

--- Admin/AbstractAdmin.php
+++ Admin/AbstractAdmin.php
@@ -14,6 +14,13 @@ use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
 class AbstractAdmin extends CogitowebAbstractAdmin
 {
 	/**
+	 * Fields to show in added object informations
+	 * 
+	 * @var string[]
+	 */
+	protected $objectInformationFields = ['id', 'createdBy', 'updatedBy', 'createdAt', 'updatedAt'];
+
+	/**
 	 * Overridden method to use Cogitoweb's ProxyQuery.
 	 * See {@see Cogitoweb\TemplateBundle\Datagrid\ProxyQuery} for further informations.
 	 * 
@@ -50,5 +57,77 @@ class AbstractAdmin extends CogitowebAbstractAdmin
 	public function getService($service)
 	{
 		return $this->getConfigurationPool()->getContainer()->get($service);
+	}
+
+	
+
+	/**
+	 * Append object informations, if available, to current ShowMapper:
+	 * - id;
+	 * - createdBy;
+	 * - updatedBy;
+	 * - createdAt;
+	 * - updatedAt;
+	 * 
+	 * @param ShowMapper $showMapper
+	 * @param string     $tabName
+	 * @param string[]   $tabOptions
+	 * @param string     $withName
+	 * @param string[]   $withOptions
+	 * @param string[]   $objectInformationFields
+	 */
+	public function addObjectInformations(ShowMapper $showMapper, $tabName = null, array $tabOptions = [], $withName = 'label.object_informations', array $withOptions = [], array $objectInformationFields = ['id', 'createdBy', 'updatedBy', 'createdAt', 'updatedAt'])
+	{
+		$entityManager = $this->getEntityManager();
+		$className     = $this->getClass();
+		$classMetadata = $entityManager->getClassMetadata($className);
+		$fieldNames    = $classMetadata->getFieldNames();
+
+		// Check if model has at least one element to show
+		if (empty(array_intersect($objectInformationFields, $fieldNames))) {
+			return;
+		}
+
+		// Open tab
+		if (null !== $tabName) {
+			// Check if it's necessary to close previous tab
+			if ($showMapper->hasOpenTab()) {
+				$showMapper->end();
+			}
+
+			$showMapper->tab($tabName, $tabOptions);
+		}
+
+		// Open group
+		if (null !== $withName) {
+			$showMapper->with($withName, $withOptions);
+		}
+
+		// Add fields
+		foreach ($objectInformationFields as $objectInformationField) {
+			if (in_array($objectInformationField, $fieldNames)) {
+				$showMapper->add($objectInformationField);
+			}
+		}
+
+		// Close group
+		if (null !== $withName) {
+			$showMapper->end();
+		}
+
+		// Close tab
+		if (null !== $tabName) {
+			$showMapper->end();
+		}
+	}
+
+	/**
+	 * Get EntityManager
+	 * 
+	 * @return EntityManagerInterface
+	 */
+	protected function getEntityManager()
+	{
+		return $this->getConfigurationPool()->getContainer()->get('doctrine.orm.entity_manager');
 	}
 }
