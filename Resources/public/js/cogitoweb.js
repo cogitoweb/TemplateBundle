81627c6c37c2a52d0eac8878c47a084607597bc7
Daniele Artico <daniele.artico@cogitoweb.it>
2016-11-23 11:03:56 +0100

    External js and css

---
+++ Resources/public/js/cogitoweb.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function () {
+	/*
+	 * Cogitoweb: AJAX loader
+	 * 
+	 * To start the loader add class "loader" to body
+	 */
+	$(document).on({
+		ajaxStart : function() { $('body').addClass   ('loader'); },
+		ajaxStop  : function() { $('body').removeClass('loader'); },
+		submit    : function() { $('body').addClass   ('loader'); }
+	});
+	/* AJAX loader */
+
+	/*
+	 * Cogitoweb: sidebar status
+	 * 
+	 * Remember last used sidebar status
+	 */
+	// Restore status
+	if ('true' == $.cookie('sidebar-collapsed')) {
+		$('.sidebar-toggle').click();
+	}
+
+	// Save status
+	$('.sidebar-toggle').on('click', function(event) {
+		if ('undefined' == typeof($.cookie('sidebar-collapsed')) || 'false' == $.cookie('sidebar-collapsed')) {
+			$.cookie('sidebar-collapsed', 'true',  { path: '/' });
+		} else {
+			$.cookie('sidebar-collapsed', 'false', { path: '/' });
+		}
+	});
+	/* Sidebar status */
+});
