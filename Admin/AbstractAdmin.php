0661552543f96362e17f9cc774a647b6c47e7ca0
Daniele Artico <daniele.artico@cogitoweb.it>
2016-11-25 14:27:07 +0100

    Added EntityManagerInterface use statement

--- Admin/AbstractAdmin.php
+++ Admin/AbstractAdmin.php
@@ -4,6 +4,7 @@ namespace Cogitoweb\TemplateBundle\Admin;
 
 use Cogitoweb\MultiLevelAdminBundle\Admin\AbstractAdmin as CogitowebAbstractAdmin;
 use Cogitoweb\TemplateBundle\Datagrid\ProxyQuery;
+use Doctrine\ORM\EntityManagerInterface;
 use Sonata\AdminBundle\Datagrid\ProxyQueryInterface;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\AdminBundle\Show\ShowMapper;
